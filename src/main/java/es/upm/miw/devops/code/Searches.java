ef65a915dea759d5e6d097bb1e4fbe589e1f2176
ainapolo <ainapolo@gmail.com>
2025-09-25 15:50:18 +0200

    definition of findFractionSubtractionByUserName #11

---
+++ src/main/java/es/upm/miw/devops/code/Searches.java
@@ -0,0 +1,14 @@
+package es.upm.miw.devops.code;
+
+import java.util.Objects;
+import java.util.stream.Stream;
+
+public class Searches {
+    public Fraction findFractionSubtractionByUserName(String name) {
+        return new UsersDatabase().findAll()
+                .filter(user -> name.equals(user.getName()))
+                .flatMap(user -> user.getFractions().stream())
+                .reduce((f1, f2) -> f1.add(new Fraction(-f2.getNumerator(), f2.getDenominator())))
+                .orElse(null);
+    }
+}
